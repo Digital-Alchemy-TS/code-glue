7a9e77d8ee7b34e88c4e745f352fc391b91ced78
Zoe Codez <github@zoe-codez.tools>
2024-10-30 13:27:18 -0500

    wip

--- src/coordinator/services/context.service.ts
+++ src/coordinator/services/context.service.ts
@@ -1,15 +1,63 @@
-import { TServiceParams } from "@digital-alchemy/core";
+import {
+  COERCE_CONTEXT,
+  GetApis,
+  InternalDefinition,
+  LIB_BOILERPLATE,
+  TServiceParams,
+} from "@digital-alchemy/core";
+import { LIB_HASS } from "@digital-alchemy/hass";
 
-export function ContextBuilder() {
-  function build(): TServiceParams {
-    // logger
-    // context
-    // hass
-    // automation (partial)
-    // shared (variables)
-    // scheduler
-    // helpers (entities)
-    return undefined;
+const REAL_BOILERPLATE = (internal: InternalDefinition) =>
+  internal.boot.loadedModules.get("boilerplate") as GetApis<
+    typeof LIB_BOILERPLATE
+  >;
+
+type tHass = GetApis<typeof LIB_HASS>;
+
+export function ContextBuilder({
+  logger,
+  als,
+  config,
+  internal,
+}: TServiceParams) {
+  function patchHass(hass: tHass) {
+    const refs = new Set();
+    // hass.refBy.id =
+    return hass;
+  }
+
+  function modulePatcher(project: string, value: unknown) {
+    switch (project as keyof TServiceParams) {
+      case "hass": {
+        return [project, patchHass(value as tHass)];
+      }
+      case "synapse": {
+        return [project, undefined];
+      }
+      default: {
+        return [project, value];
+      }
+    }
+  }
+
+  function build(name: string): TServiceParams {
+    const context = COERCE_CONTEXT(`dynamic:${name}`);
+    return {
+      ...Object.fromEntries(
+        [...internal.boot.loadedModules.keys()].map(project =>
+          modulePatcher(project, internal.boot.loadedModules.get(project)),
+        ),
+      ),
+      als,
+      config,
+      context,
+      event: internal?.utils?.event,
+      internal,
+      // lifecycle doesn't really make sense in this context
+      lifecycle: undefined,
+      logger: internal.boilerplate.logger.context(context),
+      scheduler: REAL_BOILERPLATE(internal)?.scheduler?.(context),
+    } as TServiceParams;
   }
   return { build };
 }
