1b56779d3d72eba1ea3feef683fb6e7d79d8b73a
Zoe Codez <github@zoe-codez.tools>
2024-11-01 18:06:47 -0500

    stuff

--- apps/server/src/utils/contracts/variables.ts
+++ apps/server/src/utils/contracts/variables.ts
@@ -3,7 +3,13 @@ import { Type } from "@sinclair/typebox";
 export const SharedVariables = Type.Object(
   {
     createDate: Type.Date(),
+    documentation: Type.String({
+      description: "User provided markdown notes",
+    }),
     id: Type.String(),
+    labels: Type.Array(Type.String(), {
+      description: "Home Assistant label_id",
+    }),
     lastUpdate: Type.Date(),
     title: Type.String({ description: "Human readable title" }),
     type: Type.String({ description: "Declared type definition" }),
