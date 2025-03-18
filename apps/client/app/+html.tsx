f75255872038ffcda208fb5e82d5a59ba2ce52aa
Chris Drackett <chris@drackett.com>
2025-03-18 14:40:10 -0700

    pull css out of paradigm

--- apps/client/app/+html.tsx
+++ apps/client/app/+html.tsx
@@ -1,6 +1,6 @@
 import { ScrollViewStyleReset } from 'expo-router/html'
+import { tamaguiConfig } from 'paradigm'
 import { type PropsWithChildren } from 'react'
-
 /**
  * This file is web-only and used to configure the root HTML for every web page during static rendering.
  * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
@@ -20,20 +20,10 @@ export default function Root({ children }: PropsWithChildren) {
         <ScrollViewStyleReset />
 
         {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
-        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
+        <style dangerouslySetInnerHTML={{ __html: tamaguiConfig.getCSS() }} />
         {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
       <body>{children}</body>
     </html>
   )
 }
-
-const responsiveBackground = `
-body {
-  background-color: #fff;
-}
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: #000;
-  }
-}`
