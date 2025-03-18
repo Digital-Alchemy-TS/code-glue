9ad2bebd897e1b29a019ced822c48fcde97bfde0
Chris Drackett <chris@drackett.com>
2025-03-18 14:40:32 -0700

    update files for paradigm

--- apps/client/babel.config.js
+++ apps/client/babel.config.js
@@ -21,6 +21,7 @@ module.exports = function (api) {
           components: ['paradigm', 'tamagui'],
           config: '../../packages/paradigm/config/tamagui.config.ts',
           logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
         },
       ],
       'react-native-reanimated/plugin',
