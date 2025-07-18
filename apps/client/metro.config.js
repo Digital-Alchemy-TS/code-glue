767a2a24fc63d48588f8fa7bb5600c1472d2ab07
Chris Drackett <chris@drackett.com>
2025-07-17 19:58:02 -0700

    revert metro config

--- apps/client/metro.config.js
+++ apps/client/metro.config.js
@@ -2,26 +2,26 @@
 /**
  * @type {import('expo/metro-config').MetroConfig}
  */
+const path = require('path')
+
 const { getDefaultConfig } = require('expo/metro-config')
 
-const path = require("path")
-
 // Find the project and workspace directories
-const projectRoot = __dirname;
+const projectRoot = __dirname
 // This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, '../..');
+const monorepoRoot = path.resolve(projectRoot, '../..')
 
 const config = getDefaultConfig(projectRoot, {
   // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true
+  isCSSEnabled: true,
 })
 
 config.watchFolders = [monorepoRoot]
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'mts', 'mjs'];
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mts', 'mjs']
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
-];
+]
 
 // Enable Tamagui and add nice web support with optimizing compiler + CSS extraction
 const { withTamagui } = require('@tamagui/metro-plugin')
