e9c8dd350468efe16f577a3ea9ab7b66e362f4a0
Chris Drackett <chris@drackett.com>
2025-12-06 15:07:16 -0800

    add initial prettier formating

--- apps/client/src/components/Editor/init.ts
+++ apps/client/src/components/Editor/init.ts
@@ -2,32 +2,48 @@ import { loader } from "@monaco-editor/react"
 import { shikiToMonaco } from "@shikijs/monaco"
 import * as monaco from "monaco-editor"
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker"
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker"
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker"
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker"
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker"
+import { configureMonacoPrettier } from "monaco-prettier"
 import { createHighlighter } from "shiki"
 
 import { appConfig } from "@/config"
+import prettierWorker from "./prettier.worker?worker"
 
 // Load Monaco locally vs. via the CDN
 self.MonacoEnvironment = {
 	getWorker(_, label) {
-		if (label === "json") {
-			return new jsonWorker()
+		switch (label) {
+			case "json":
+				return new jsonWorker()
+			case "javascript":
+			case "typescript":
+				return new tsWorker()
+			case "prettier":
+				return new prettierWorker()
+			default:
+				return new editorWorker()
 		}
-		if (label === "css" || label === "scss" || label === "less") {
-			return new cssWorker()
-		}
-		if (label === "html" || label === "handlebars" || label === "razor") {
-			return new htmlWorker()
-		}
-		if (label === "typescript" || label === "javascript") {
-			return new tsWorker()
-		}
-		return new editorWorker()
 	},
 }
+
+// # Configure Prettier
+
+const printWidth = 80
+configureMonacoPrettier(monaco, {
+	// Map Monaco language IDs to a Prettier parser
+	parsers: {
+		typescript: "babel",
+	},
+
+	// Prettier options.
+	prettier: {
+		printWidth,
+		semi: false,
+	},
+})
+
+// # Syntax Highlighting Setup
 
 // Create the language highlighter, add themes and languages in appConfig
 const highlighter = await createHighlighter({
@@ -35,7 +51,7 @@ const highlighter = await createHighlighter({
 	langs: appConfig.editor.languages,
 })
 
-// Register Languages with Monaco
+// Register Languages with Monaco for syntax highlighting
 appConfig.editor.languages.forEach((language) => {
 	monaco.languages.register({ id: language })
 })
