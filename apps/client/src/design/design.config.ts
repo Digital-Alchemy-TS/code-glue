a37d0dc8c2644ddfa5166eb071b46e1232beb619
Chris Drackett <chris@drackett.com>
2025-12-16 12:36:12 -0800

    remove theme overrides

--- apps/client/src/design/design.config.ts
+++ apps/client/src/design/design.config.ts
@@ -2,21 +2,6 @@ import type { ParadigmConfig } from "@code-glue/paradigm"
 
 export const glueDesignConfig = {
 	size: {
-		headerHeight: 60,
-	},
-	themes: {
-		light: {
-			primary: "#367FEE",
-			destructive: "#367FEE",
-			color: "#367FEE",
-			secondaryColor: "#777D86",
-			disabledColor: "#F7E087",
-			placeholderColor: "#B2B4B9",
-			uiStroke: "#E9EBED",
-			cardStock: "#F9F9F9",
-			background: "#FFFFFF",
-			controlShadow: "rgba(18, 18, 18, 0.06)",
-			holeShadow: "rgba(18, 18, 18, 0.24)",
-		},
+		headerHeight: 56,
 	},
 } satisfies ParadigmConfig
