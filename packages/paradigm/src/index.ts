a11e43615f0830987203d9408203b94100f11670
Chris Drackett <chris@drackett.com>
2026-01-09 15:10:43 -0800

    export scrollview

--- packages/paradigm/src/index.ts
+++ packages/paradigm/src/index.ts
@@ -4,12 +4,15 @@ export * from "./config/paradigm.config"
 
 // Components
 
+// Components
+
 export * from "./components/Button"
 export * from "./components/Icon"
 export * from "./components/Layout"
 export * from "./components/List"
 export * from "./components/ListItem"
 export * from "./components/ParadigmProvider"
+export * from "./components/ScrollView"
 export * from "./components/Text"
 export * from "./components/TextInput"
 export * from "./components/View"
