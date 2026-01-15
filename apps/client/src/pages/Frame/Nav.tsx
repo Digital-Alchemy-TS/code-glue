ce118e9db397ee6dbe5d75f173cf1c1446d3985e
Chris Drackett <chris@drackett.com>
2026-01-15 14:24:01 -0800

    add query for settings

--- apps/client/src/pages/Frame/Nav.tsx
+++ apps/client/src/pages/Frame/Nav.tsx
@@ -1,3 +1,4 @@
+import { parseAsBoolean, useQueryState } from "nuqs"
 import { useSnapshot } from "valtio"
 
 import { Button, Column, Icon, List, ListItem } from "@code-glue/paradigm"
@@ -5,43 +6,59 @@ import { useRouter } from "@/hooks/useRouter"
 import { store } from "@/store"
 import { CreateAutomation } from "../../components/CreateAutomation"
 import { Header } from "../../components/Header"
+import { Settings } from "./Settings"
 export const Nav = () => {
 	const { automations } = useSnapshot(store)
 
 	const [{ route, automationId }, navigateTo] = useRouter()
 
+	const [isSettings, setIsSettings] = useQueryState(
+		"settings",
+		parseAsBoolean.withDefault(false),
+	)
+
 	return (
 		<Column grow color={"$cardStock"}>
 			<Header>
 				<img src="./headerLogo.png" alt="CodeGlue" height={42} />
-				<Button icon={Icon.Settings} isRaised />
+				<Button
+					icon={isSettings ? Icon.X : Icon.Settings}
+					isRaised
+					onPress={() => {
+						setIsSettings(!isSettings)
+					}}
+				/>
 			</Header>
-			<List.Group>
-				<List.Simple>
-					<ListItem
-						label="Logs"
-						isSelected={!route}
-						onPress={() => navigateTo("home")}
-					/>
-				</List.Simple>
-				<List.Simple header="Automations">
-					<CreateAutomation />
-					{Array.from(automations, ([, automation]) => {
-						return (
-							<ListItem
-								label={automation.title}
-								key={automation.id}
-								isSelected={
-									route === "automations" && automation.id === automationId
-								}
-								onPress={() => {
-									navigateTo("automations", { automationId: automation.id })
-								}}
-							/>
-						)
-					})}
-				</List.Simple>
-			</List.Group>
+			{isSettings ? (
+				<Settings />
+			) : (
+				<List.Group>
+					<List.Simple>
+						<ListItem
+							label="Logs"
+							isSelected={!route}
+							onPress={() => navigateTo("home")}
+						/>
+					</List.Simple>
+					<List.Simple header="Automations">
+						<CreateAutomation />
+						{Array.from(automations, ([, automation]) => {
+							return (
+								<ListItem
+									label={automation.title}
+									key={automation.id}
+									isSelected={
+										route === "automations" && automation.id === automationId
+									}
+									onPress={() => {
+										navigateTo("automations", { automationId: automation.id })
+									}}
+								/>
+							)
+						})}
+					</List.Simple>
+				</List.Group>
+			)}
 		</Column>
 	)
 }
---
+++ apps/client/src/pages/Frame/Settings.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@code-glue/paradigm"
+
+export const Settings = () => {
+	return <Text>Settings</Text>
+}
