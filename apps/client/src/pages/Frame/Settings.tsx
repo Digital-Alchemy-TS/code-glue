5ec24f68ffefa7b259c39eb5f57ec106ab9132a1
Chris Drackett <chris@drackett.com>
2026-02-03 10:39:20 -0800

    placeholder settings

--- apps/client/src/pages/Frame/Settings.tsx
+++ apps/client/src/pages/Frame/Settings.tsx
@@ -1,5 +1,22 @@
-import { Text } from "@code-glue/paradigm"
+import { Column, Text } from "@code-glue/paradigm"
 
 export const Settings = () => {
-	return <Text>Settings</Text>
+	return (
+		<Column>
+			<Text>Settings</Text>
+			<Text> Appearance (Dark/light/system) dropdown</Text>
+			<Text>------</Text>
+			<Text>Editor Theme (dropdown)</Text>
+			<Text>Typeface (dropdown)</Text>
+			<Text>Font Size (slider)</Text>
+			<Text>Width (slider)</Text>
+			<Text>Weight (slider)</Text>
+			<Text>------</Text>
+			<Text>Console Theme (dropdown)</Text>
+			<Text>Typeface (dropdown)</Text>
+			<Text>Font Size (slider)</Text>
+			<Text>Width (slider)</Text>
+			<Text>Weight (slider)</Text>
+		</Column>
+	)
 }
